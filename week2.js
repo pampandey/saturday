ed90cfcec58051e105ac01ad88bfcb78d50d8657
Pramila Pandey <pam.pandey@Pramilas-MacBook-Air.local>
2022-07-23 11:46:34 -0400

    magic functions

---
+++ week2.js
@@ -0,0 +1,27 @@
+let students = [ //array
+    {//object
+        name: "maxxxx",
+        here: true
+    },
+    {
+    name: "pam",
+    here: true
+}
+]
+
+
+
+for (let = i=0;t<10;i++) {
+
+}function getStudentsNames(studentsArray) {
+    let studentName = []
+    for (let i=0; i<studentsArray.length;1++) {
+         studentName.push[i].(studentsArray[i].name)
+      
+    }
+    return studentName;
+}
+
+
+console.log(getStudentsNames(students))
+
